a96324b9f5c838d0fc22788ee4586a3f5583d15b
joeyq <jq@terrorist.me>
2025-07-11 05:13:13 +0300

    title update 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
+// app/layout.tsx
 import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: "All Hail Lord Rath of the bone",
-  description: "A shrine forged in pixels and vengeance.",
-  generator: 'All Hail Lord Rath of the bone',
+  title: 'Rathbone feeds of your tears',
+  description: 'A shrine forged in pixels and vengeance.',
+  generator: 'Rathbone Core v0',
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>{children}</body>
